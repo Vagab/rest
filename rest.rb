89a7baf426e338068d1ec4026834bd5eda1c2817
barseek <sergey.b@hey.com>
2023-05-29 17:17:00 +0200

    Sensible defaults

--- rest.rb
+++ rest.rb
@@ -49,10 +49,10 @@ def parse_time(str)
 end
 
 def start(options)
-  dim_duration = options[:dim_duration] || 10
-  sleep_duration = options[:sleep_duration] || 3600
-  brightness = options[:brightness] || 0.1
-  inactive = options[:inactive] || 30
+  dim_duration = options[:dim_duration]
+  sleep_duration = options[:sleep_duration]
+  brightness = options[:brightness]
+  inactive = options[:inactive]
   run_in_background = options[:run_in_background] || false
 
   if run_in_background
@@ -116,7 +116,7 @@ option_parser = OptionParser.new do |opts|
       puts 'Dim duration must be a positive integer not exceeding 300 (5 minutes)'
       exit
     end
-    options[:dim_duration] = d
+    options[:dim_duration] = d || 30
   end
 
   opts.on('-sSLEEP', '--sleep=SLEEP', 'Set sleep duration') do |s|
@@ -125,7 +125,7 @@ option_parser = OptionParser.new do |opts|
       puts 'Sleep duration must be a positive integer'
       exit
     end
-    options[:sleep_duration] = s
+    options[:sleep_duration] = s || 3600
   end
 
   opts.on('-bBRIGHTNESS', '--brightness=BRIGHTNESS', 'Set dimming brightness') do |b|
@@ -134,7 +134,7 @@ option_parser = OptionParser.new do |opts|
       puts 'Brightness must be a float between 0.0 and 1.0'
       exit
     end
-    options[:brightness] = b
+    options[:brightness] = b || 0.1
   end
 
   opts.on('-iINACTIVE', '--inactive=INACTIVE', 'Set the duration of inactivity before dimming') do |i|
@@ -143,7 +143,7 @@ option_parser = OptionParser.new do |opts|
       puts 'Inactive duration must be a positive integer'
       exit
     end
-    options[:inactive] = i
+    options[:inactive] = i || 30
   end
 
   opts.on('-r', '--run-in-background', 'Run in background') do
